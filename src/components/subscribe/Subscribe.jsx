1d257114e4673853acb87ea02de6f6e7933ba50e
Muhammadrasul21 <theboy6878@gmail.com>
2024-10-30 00:02:51 +0500

    ij

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../subscribe/Subscribe.scss";
-import subscribeWatch from "../../../public/subscribeWatch.png";
+import subscribeWatch from "/subscribeWatch.png";
 const Subscribe = () => {
   return (
     <div className="subscribe">

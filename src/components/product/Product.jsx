427c17c8128759834a54ff7f562a3532fdbe18d0
Muhammadrasul21 <theboy6878@gmail.com>
2024-10-29 20:17:48 +0500

    edited product

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -27,26 +27,45 @@ const PRODUCTS = [
   },
   {
     id: 4,
-    title: "Apple Smart III",
-    price: "$400.00",
-    info: "$355.00",
+    title: "Apple Smart IV",
+    price: "$450.00",
+    info: "$395.00",
     img: appleSmart,
   },
   {
     id: 5,
-    title: "Apple Smart III",
-    price: "$400.00",
-    info: "$355.00",
+    title: "Apple Smart V",
+    price: "$500.00",
+    info: "$455.00",
     img: appleSmart,
   },
   {
     id: 6,
-    title: "Apple Smart III",
-    price: "$400.00",
-    info: "$355.00",
+    title: "Apple Smart VI",
+    price: "$550.00",
+    info: "$505.00",
     img: appleSmart,
   },
 ];
+
+const ProductCard = ({ item }) => (
+  <div className="product__card">
+    <div className="product__img">
+      <img src={item.img} alt={item.title} />
+    </div>
+    <div className="product__info">
+      <p>{item.title}</p>
+      <div className="product__rating">
+        {[...Array(5)].map((_, index) => (
+          <FaStar key={index} />
+        ))}
+      </div>
+      <div className="product__price">
+        <del>{item.price}</del> <p>{item.info}</p>
+      </div>
+    </div>
+  </div>
+);
 
 const Product = () => {
   return (
@@ -58,22 +77,7 @@ const Product = () => {
         </div>
         <div className="product__wrapper">
           {PRODUCTS.map((item) => (
-            <div key={item.id} className="product__card">
-              <div className="product__img">
-                <img src={item.img} alt={item.title} />
-              </div>
-              <div className="product__info">
-                <p>{item.title}</p>
-                <div className="product__rating">
-                  {[...Array(5)].map((_, index) => (
-                    <FaStar key={index} />
-                  ))}
-                </div>
-                <div className="product__price">
-                  <del>{item.price}</del> <p>{item.info}</p>
-                </div>
-              </div>
-            </div>
+            <ProductCard key={item.id} item={item} />
           ))}
         </div>
         <button className="btn prdBtn">View More</button>
